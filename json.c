529fb507ba2e0f93861612b4035a1c93e7d8e698
D.Roukin <D.Roukin.Jobs@gmail.com>
2018-04-19 06:58:42 +1200

    added json files, GOING TO REWRITE MAIN for PUT requests

---
+++ json.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "json.h"
+
+
+void json2term(char* str_json, char* str_term){
+	//supposed to translate json to term
+}
+
+void term2json(terminal_type term, char* str_json){
+	//convert terminal data to json
+	
+}	
---
+++ json.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+void json2term(char* str_json, char* str_term); 
+void term2json(char* str_term, char* str_json); 
